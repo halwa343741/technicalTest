a0059f9e9f490a656b1c45166fa30d43aca91e9b
halwa343741 <tanory.1407@gmail.com>
2024-07-03 17:21:23 +0700

    Vehicle Rest API SQL

---
+++ VehicleRESTAPI.sql
@@ -0,0 +1,126 @@
+/****** Object:  Table [dbo].[pricelist]    Script Date: 03-Jul-24 17:18:39 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[pricelist](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[code] [varchar](50) NOT NULL,
+	[price] [decimal](18, 2) NOT NULL,
+	[year_id] [int] NOT NULL,
+	[model_id] [int] NOT NULL,
+	[created_at] [datetime] NOT NULL,
+	[updated_at] [datetime] NOT NULL,
+ CONSTRAINT [PK_pricelist] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[users]    Script Date: 03-Jul-24 17:18:39 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[users](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](50) NOT NULL,
+	[is_admin] [bit] NOT NULL,
+	[created_at] [datetime] NOT NULL,
+	[updated_at] [datetime] NOT NULL,
+ CONSTRAINT [PK_users] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[vehicle_brand]    Script Date: 03-Jul-24 17:18:39 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[vehicle_brand](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](50) NOT NULL,
+	[created_at] [datetime] NOT NULL,
+	[updated_at] [datetime] NOT NULL,
+ CONSTRAINT [PK_vehicle_brand] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[vehicle_model]    Script Date: 03-Jul-24 17:18:39 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[vehicle_model](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](50) NOT NULL,
+	[type_id] [int] NOT NULL,
+	[created_at] [datetime] NOT NULL,
+	[updated_at] [datetime] NOT NULL,
+ CONSTRAINT [PK_vehicle_model] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[vehicle_type]    Script Date: 03-Jul-24 17:18:39 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[vehicle_type](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](50) NOT NULL,
+	[brand_id] [int] NULL,
+	[created_at] [datetime] NOT NULL,
+	[updated_at] [datetime] NOT NULL,
+ CONSTRAINT [PK_vehicle_type] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[vehicle_year]    Script Date: 03-Jul-24 17:18:39 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[vehicle_year](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[year] [int] NOT NULL,
+	[created_at] [datetime] NOT NULL,
+	[updated_at] [datetime] NOT NULL,
+ CONSTRAINT [PK_vehicle_year] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[pricelist]  WITH CHECK ADD  CONSTRAINT [FK_pricelist_vehicle_model] FOREIGN KEY([model_id])
+REFERENCES [dbo].[vehicle_model] ([id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[pricelist] CHECK CONSTRAINT [FK_pricelist_vehicle_model]
+GO
+ALTER TABLE [dbo].[pricelist]  WITH CHECK ADD  CONSTRAINT [FK_pricelist_vehicle_year] FOREIGN KEY([year_id])
+REFERENCES [dbo].[vehicle_year] ([id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[pricelist] CHECK CONSTRAINT [FK_pricelist_vehicle_year]
+GO
+ALTER TABLE [dbo].[vehicle_model]  WITH CHECK ADD  CONSTRAINT [FK_vehicle_model_vehicle_type] FOREIGN KEY([type_id])
+REFERENCES [dbo].[vehicle_type] ([id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[vehicle_model] CHECK CONSTRAINT [FK_vehicle_model_vehicle_type]
+GO
+ALTER TABLE [dbo].[vehicle_type]  WITH CHECK ADD  CONSTRAINT [FK_vehicle_type_vehicle_brand] FOREIGN KEY([brand_id])
+REFERENCES [dbo].[vehicle_brand] ([id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[vehicle_type] CHECK CONSTRAINT [FK_vehicle_type_vehicle_brand]
+GO
